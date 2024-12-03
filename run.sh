88477fd8ce1f1a32dd7c735dab8eb5a5d6cb3d93
Queen-trading <miketing3133@gmail.com>
2024-12-03 11:21:35 -0500

    update interface for openvpn

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-sudo ip link set dev eth1 mtu 4500
+sudo ip link set dev tun0 mtu 4500
 
-sudo /usr/sbin/ip link set eth1 up || true
-sudo /usr/sbin/ip link set eth1 promisc on || true
-sudo /sbin/ethtool -G eth1 rx 4096 tx 4096 || true
+sudo /usr/sbin/ip link set tun0 up || true
+sudo /usr/sbin/ip link set tun0 promisc on || true
+sudo /sbin/ethtool -G tun0 rx 4096 tx 4096 || true
 
-sudo /sbin/ethtool -K eth1 rx off tx off sg off tso off ufo off gso off gro off lro off || true
+sudo /sbin/ethtool -K tun0 rx off tx off sg off tso off ufo off gso off gro off lro off || true
 
 sudo docker-compose up -d --build
